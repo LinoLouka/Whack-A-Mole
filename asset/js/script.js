20ffa99661682a068dbffbf33fd52840e236c471
L0uka <loukacadaulino@gmail.com>
2023-05-02 14:28:23 +0200

    add js

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,12 +1,43 @@
 //récupération des données html
-const score = document.querySelector("div.score");
-const circle = document.querySelectorAll("div.circle");
+const circles = document.querySelectorAll("div.circle");
+const score = document.querySelector("span");
 
-//Création du texte de score
-let point; //point gagné dans le jeu
-const text = document.createElement("p");
-text.className = "texte";
-score.appendChild(text);
-text.textContent = "score: " + point;
+// Fonction pour sélectionner un élément de cercle aléatoire
+function selectRandomCircle() {
+  // Supprimez la classe "mole" de tous les cercles
+  circles.forEach((circle) => {
+    circle.classList.remove("mole");
+  });
 
-//Event d'apparation des taupes
+  // Générez un index aléatoire pour sélectionner un élément de cercle
+  const randomIndex = Math.floor(Math.random() * circles.length);
+
+  // Récupérez l'élément de cercle correspondant à l'index aléatoire
+  const randomCircle = circles[randomIndex];
+
+  // Ajoutez la classe "mole" à l'élément de cercle sélectionné
+  randomCircle.classList.add("mole");
+
+  // Supprimez la classe "mole" de l'élément de cercle après un délai aléatoire
+  setTimeout(() => {
+    randomCircle.classList.remove("mole");
+  }, Math.random() * 3000 + 1000); // Délai aléatoire entre 1 et 4 secondes
+}
+
+// Appelez la fonction selectRandomCircle() à intervalles réguliers pour sélectionner un cercle aléatoire
+setInterval(selectRandomCircle, 1000); // Sélectionnez un cercle toutes les 1 seconde
+
+// Ajouter un écouteur d'événement "click" à chaque cercle
+circles.forEach((circle) => {
+  circle.addEventListener("click", () => {
+    if (circle.classList.contains("mole")) {
+      // Ajouter 1 point au score
+      let currentScore = parseInt(score.textContent);
+      currentScore++;
+      score.textContent = currentScore < 10 ? `0${currentScore}` : currentScore;
+
+      // Supprimer la classe "mole" du cercle pour le faire disparaître
+      circle.classList.remove("mole");
+    }
+  });
+});
