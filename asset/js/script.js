572eeb8afa7567080b9788f75aa3d126eb3eabd5
L0uka <loukacadaulino@gmail.com>
2023-05-02 14:41:39 +0200

    modification commentaire

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,42 +1,42 @@
-//Récupération des données html
+// Récupération des données html
 const circles = document.querySelectorAll("div.circle");
 const score = document.querySelector("span");
 
-// Fonction pour sélectionner un élément de cercle aléatoire
+// Séléction aléatoire d'un élément de cercle
 function selectRandomCircle() {
   // Supprimez la classe "mole" de tous les cercles
   circles.forEach((circle) => {
     circle.classList.remove("mole");
   });
 
-  // Générez un index aléatoire pour sélectionner un élément de cercle
+  // Générez un randomIndex pour sélectionner
   const randomIndex = Math.floor(Math.random() * circles.length);
 
-  // Récupérez l'élément de cercle correspondant à l'index aléatoire
+  // Récupérez l'élément de cercle correspondant à randomIndex
   const randomCircle = circles[randomIndex];
 
-  // Ajoutez la classe "mole" à l'élément de cercle sélectionné
+  // Ajoutez la classe "mole"
   randomCircle.classList.add("mole");
 
-  // Supprimez la classe "mole" de l'élément de cercle après un délai aléatoire
+  // Supprimez la classe "mole"
   setTimeout(() => {
     randomCircle.classList.remove("mole");
   }, Math.random() * 3000 + 1000); // Délai aléatoire entre 1 et 4 secondes
 }
 
-// Appelez la fonction selectRandomCircle() à intervalles réguliers pour sélectionner un cercle aléatoire
-setInterval(selectRandomCircle, 1000); // Sélectionnez un cercle toutes les 1 seconde
+// Appelez la fonction selectRandomCircle() à intervalles réguliers
+setInterval(selectRandomCircle, 1000); // Sélectionnez un cercle toutes les seconde
 
-// Ajouter un écouteur d'événement "click" à chaque cercle
+// Ajout d'un événement "click" à chaque cercle
 circles.forEach((circle) => {
   circle.addEventListener("click", () => {
     if (circle.classList.contains("mole")) {
-      // Ajouter 1 point au score
+      // Ajouter 10 point au score
       let currentScore = parseInt(score.textContent);
-      currentScore++;
+      currentScore += 10;
       score.textContent = currentScore < 10 ? `0${currentScore}` : currentScore;
 
-      // Supprimer la classe "mole" du cercle pour le faire disparaître
+      // Supprimer la classe "mole"
       circle.classList.remove("mole");
     }
   });
